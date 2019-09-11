ddd93a034ad68f26b6dbb3045c799ce7510437e5
lucy <lucy@lucydeMacBook-Pro.local>
2019-09-11 13:08:11 +0800

    add report function

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask
+import unittest
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,5 +13,11 @@ def hello_world():
 def hello_user(username):
     return 'Why Hello %s!\n' % username
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')     # open for everyone
+    ############# Add these lines #############
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    ###########################################
+    unittest.main()
